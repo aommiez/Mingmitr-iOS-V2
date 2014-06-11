44cf6ab533cd00e285d727ea5e89cf28ebb13aa5
aommiez <aommiez@gmail.com>
2014-06-11 10:48:03 +0700

    init project

---
+++ MingMitr/MingMitr/PFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PFAppDelegate.h
+//  MingMitr
+//
+//  Created by MRG on 6/11/2557 BE.
+//  Copyright (c) 2557 Platwo fusion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MingMitr/MingMitrTests/MingMitrTests.m
@@ -0,0 +1,34 @@
+//
+//  MingMitrTests.m
+//  MingMitrTests
+//
+//  Created by MRG on 6/11/2557 BE.
+//  Copyright (c) 2557 Platwo fusion. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MingMitrTests : XCTestCase
+
+@end
+
+@implementation MingMitrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
